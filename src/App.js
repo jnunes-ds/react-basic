4daf3ebe8f692d9c76ef26afd0ee6ee6e30b464a
Júnior Nunes <denisonprobable13@gmail.com>
2020-10-19 23:48:02 -0300

    created a new component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Comentario from './components/Comentario';
+import comentario from './components/Comentario'
 
 function App() {
   return (
     <div className="App">
       <h1>Meu projeto</h1>
+      <Comentario />
     </div>
   );
 }
--- src/components/Comentario.js
+++ src/components/Comentario.js
@@ -1,1 +1,8 @@
 import React from 'react'
+
+//JSX
+const Comentario = () => (
+    <div>Olá, eu sou um componente</div>
+);
+
+export default Comentario
