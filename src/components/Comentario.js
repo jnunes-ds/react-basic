7bc3a7c302ed8f413d1342a6026508d78025d38f
Júnior Nunes <denisonprobable13@gmail.com>
2020-10-20 00:16:00 -0300

    Added to Comentario.js

--- src/components/Comentario.js
+++ src/components/Comentario.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './Comentario.css'
 
 //JSX
 const Comentario = () => (
-    <>
+    <div class="Comentario">
         <div>João:</div>
         <div>Olá, tudo bem?</div>
-    </>
+    </div>
 );
 
 export default Comentario
